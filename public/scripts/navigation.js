64dd53a8a3257e83f03412f2de414129a51321fe
Glauber Ramos <glauberamos@gmail.com>
2013-01-03 12:07:46 -0200

    changes on navigation and html structure for retrospective checkin

--- public/scripts/navigation.js
+++ public/scripts/navigation.js
@@ -1,12 +1,12 @@
 var FormNavigator = function() {
-	nextField = function() {
+	nextSection = function() {
 		if(!$('.doing').is(':last-child')) {
 			$('.doing').addClass('done').removeClass('doing')
 				.next().addClass('doing').removeClass('to-do');
 		}
 	};
 
-	previousField = function() {
+	previousSection = function() {
 		if(!$('.doing').is(':first-child')) {
 			$('.doing').addClass('to-do').removeClass('doing')
 				.prev().addClass('doing').removeClass('done');
@@ -16,13 +16,13 @@ var FormNavigator = function() {
 	/*TAB 9 ENTER 13 ESC 27 UP 40 DOWN  38 */
 	 $('body').keydown(function(key) {
 	        if(key.which == 9) {
-	            key.preventDefault();
-	            nextField();
+	            //key.preventDefault();
+	            nextSection();
 	        }
 
 	        if(key.which == 27) {
-	            key.preventDefault();
-	            previousField();
+	            //key.preventDefault();
+	            previousSection();
 	        }
 	 });
 }
